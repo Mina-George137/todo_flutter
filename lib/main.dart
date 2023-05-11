f45bfa1c9bc556d244ed3c7d7db031336fa7d9ae
Mina-George137 <minageorge137@gmail.com>
2023-05-11 15:17:09 +0300

    initial project Created empty home screen

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/home/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+      },
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
